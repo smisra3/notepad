b8b9b1e716d5eb74b5e9b47e8c6307fe2408dad2
Shubhankar Misra <shubhankar.misra@99Acres.com>
2020-03-17 15:02:41 +0530

    Adding the redux store using an HOC called enhance.js

--- components/common/Footer/Footer.js
+++ components/common/Footer/Footer.js
@@ -1,6 +1,7 @@
 import Link from 'next/link';
+import { connect } from 'react-redux';
 
-import withStyles from '../../../libs/withStyles';
+import withStyles from '../../../HOC/withStyles';
 
 import styles from './Footer.style';
 
@@ -8,11 +9,15 @@ const Footer = props => {
   return (
     <ul className={props.className}>
       <li><a href="/">{'New note'}</a></li>
-      <li><Link href="/about">{'About us'}</Link></li>
-      <li><Link href="/contact">{'Contact'}</Link></li>
-      <li><Link href="/terms">{'Terms'}</Link></li>
+      <li><Link href="/about"><a>{'About us'}</a></Link></li>
+      <li><Link href="/contact"><a>{'Contact'}</a></Link></li>
+      <li><Link href="/terms"><a>{'Terms'}</a></Link></li>
     </ul>
   );
 };
 
-export default withStyles(Footer, styles);
+const mstp = state => ({
+  state,
+});
+
+export default connect(mstp)(withStyles(Footer, styles));
