875f1300651ee75e1bfaed89d118996e9fc330a7
navjot.sharma <navjotsharma999@gmail.com>
2020-03-19 16:32:53 +0530

    toolbar added

--- components/common/Toolbar/Toolbar.js
+++ components/common/Toolbar/Toolbar.js
@@ -1,8 +1,53 @@
-import React from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
+import styles from './toolbar.module.scss';
 
-const Toolbar = props => {
+const style = {
+  display: 'block',
+  width: '100%',
+  margin: '0 auto',
+  background: '#fff',
+  color: '#000',
+  fontFamily: 'Arial, Helvetica, sans-serif',
+  border: '0',
+  borderTop: '1px solid #e8e8e8',
+  borderBottom: '1px solid #e8e8e8',
+  resize: 'none',
+  outline: 'none',
+  padding: '15px',
+  lineHeight: '20px',
+  fontSize: '15px',
+  height: '477.2px',
+};
 
-  return (<div style={{ height: 100, width: 100 }} contentEditable></div>);
+const onClickHandler = (key, setAttr, prevState = {}) => {
+  let _obj = {};
+  _obj[key] = !prevState[key];
+  _obj = { ...prevState, ..._obj };
+  setAttr(_obj);
+  document.getElementById('content').focus();
+  document.execCommand(key);
+}
+
+const Toolbar = () => {
+
+  const [attr, setAttr] = useState({
+    bold: false,
+    italic: false,
+    underline: false
+  });
+  
+  useEffect(()=>{
+    document.getElementById('content').focus();
+  },[]);
+
+  return (<Fragment>
+    <div className={styles.wrapper}>
+      <button onClick={() => onClickHandler('bold', setAttr, attr)} className={`${styles.bold} ${styles.common} ${attr.bold ? styles.active : ''}`}>B</button>
+      <button onClick={() => onClickHandler('italic', setAttr, attr)} className={`${styles.common} ${attr.italic ? styles.active : ''}`}><i>I</i></button>
+      <button onClick={() => onClickHandler('underline', setAttr, attr)} className={`${styles.underline} ${styles.common} ${attr.underline ? styles.active : ''}`}>U</button>
+    </div>
+    <div style={style} id="content" contentEditable></div>
+  </Fragment>);
 }
 
 export default Toolbar;
